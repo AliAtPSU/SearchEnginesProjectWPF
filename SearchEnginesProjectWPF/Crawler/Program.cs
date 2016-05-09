2ca7aaf8c543446356eedc1dafdcc7cd3faf3357
AliChaaban <AliChaaban@DESKTOP-TIKUIQ2>
2016-05-09 22:18:21 +0300

    added crawler

---
+++ SearchEnginesProjectWPF/Crawler/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CSharpCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crawler.CrawlSite();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
